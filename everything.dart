d991b9a05953d9dbac1d4bb4337df0c82c237b42
Kalyan Biswas <40482381+Kalyanb447-github@users.noreply.github.com>
2020-02-11 16:57:20 +0530

    multiple listview builder

--- everything.dart
+++ everything.dart
@@ -1449,3 +1449,39 @@ step :2
         appBar: new AppBar(
           title: new Text(widget.title),
         ),
+        
+//multiple listview builder
+         body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Header for list $index',
+                  style: Theme.of(context).textTheme.body2,
+                ),
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 8.0,
+                      ),
+                      child: Text(
+                        'Nested list item $index',
+                        style: Theme.of(context).textTheme.body1,
+                      ),
+                    );
+                  },
+                  itemCount: 4,
+                  shrinkWrap: true, // todo comment this out and check the result
+                  physics: ClampingScrollPhysics(), // todo comment this out and check the result
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: 9,
+      ),
+    );
